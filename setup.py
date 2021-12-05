0e01f807b7389fc78a9d947139634c2de8e29421
Thomas Weng <thomas.weng11@gmail.com>
2021-12-04 23:54:56 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+import os.path as osp
+
+from setuptools import find_packages, setup
+
+requirements = []
+
+__version__ = "1.0.0"
+
+setup(
+    name="contact_graspnet",
+    version=__version__,
+    author="",
+    packages=find_packages(),
+    install_requires=requirements,
+)
